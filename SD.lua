0633a80a6f0edaccce0eb8206f3981622334d1f1
pradodis <prado.dis@gmail.com>
2020-03-23 03:50:36 +0000

    Heli-SAM bug fixed. More Sweep Added

--- SD.lua
+++ SD.lua
@@ -1,3 +1,6 @@
+-- Anti-trucks mission with random events for DCS.
+-- Created by Diego Silva Prado
+
 function runDice ()
     local dice = 0.0
     for i=1,10 do
@@ -17,15 +20,66 @@ nAAA = {5,15}
 
 runDice()
 eNum = {math.random(nIgla[1],nIgla[2]),math.random(nAK[1],nAK[2]),math.random(nFlak[1],nFlak[2]),math.random(nAAA[1],nAAA[2])}
+
+-- Listas de inimigos e Zonas
 eGroups={"E_Igla","E_AK","E_FLAK","E_AAA"}
 eZones={"AO1","AO2","AO3","AO4","AO5"}
 
--- Chance of Calling FS --
-figtherSweepInitial = 0.
-figtherSweepIncrease = 0.05
-figtherSweepLimit = 3.
-figtherSweep = 0.
-figtherSweepInterval = 30.
+-- Lista de Aeronaves
+-- Dummy -- Low -- Medium -- High
+cPlanes = {
+    ["F-15C"] = {0.0,1.0,2.0,1.0},
+    ["Su-27"] = {0.0,1.0,2.0,1.0},
+    ["MiG-29S"] = {0.0,1.0,2.0,1.0},
+    ["FA-18C_hornet"] = {0.0,1.0,2.0,1.0},
+    ["F-14B"] = {1.0,2.0,1.0,0.0},
+    ["F-5E-3"] = {2.0,1.0,0.0,0.0},
+    ["Ka-50"] = {0.0,2.0,0.0,0.0},
+    ["Mi-8MT"] = {0.0,0.5,0.0,0.0},
+    ["UH-1H"] = {0.0,0.5,0.0,0.0}
+}
+
+--------------------------------------------------------------------------------------
+-- Chance of Calling FS - Dummy
+figtherSweepDummyInitial = 0.
+figtherSweepDummy = 0.
+figtherSweepDummyInterval = 30.
+figtherSweepDummyIncrease = 0.0
+figtherSweepDummyLimit = 0.0
+-- Per Player
+figtherSweepDummyIncreasePP = 0.02
+figtherSweepDummyLimitPP = 1.5
+
+-- Chance of Calling FS - Low
+figtherSweepLowInitial = 0.
+figtherSweepLow = 0.
+figtherSweepLowInterval = 30.
+figtherSweepLowIncrease = 0.0
+figtherSweepLowLimit = 0.0
+-- Per Player
+figtherSweepLowIncreasePP = 0.02
+figtherSweepLowLimitPP = 1.5
+
+-- Chance of Calling FS - Medium
+figtherSweepMediumInitial = 0.
+figtherSweepMedium = 0.
+figtherSweepMediumInterval = 30.
+figtherSweepMediumIncrease = 0.0
+figtherSweepMediumLimit = 0.0
+-- Per Player
+figtherSweepMediumIncreasePP = 0.02
+figtherSweepMediumLimitPP = 1.5
+
+-- Chance of Calling FS - High
+figtherSweepHighInitial = 0.
+figtherSweepHigh = 0.
+figtherSweepHighInterval = 30.
+figtherSweepHighIncrease = 0.0
+figtherSweepHighLimit = 0.0
+-- Per Player
+figtherSweepHighIncreasePP = 0.02
+figtherSweepHighLimitPP = 1.5
+---------------------------------------------------------------------------------------
 
 -- Casual Events
 casualEventsInterval = 30.
@@ -33,25 +87,25 @@ activeEV = false
 
 -- CAS
 CASChanceInitial = 0.
-CASChanceIncrease = 0.05
-CASChanceLimit = 3.
+CASChanceIncrease = 0.01
+CASChanceLimit = 0.75
 CASChance = 0.
 earlyCASHelp = 300.
-middleCASHelp = 900.
-deadCASEnd = 1200.
+middleCASHelp = 600.
+deadCASEnd = 900.
 intermitencyMsgCAS = 30.
 
 -- Helis
 HelisChanceInitial = 0.
-HelisChanceIncrease = 0.05
-HelisChanceLimit = 3.
+HelisChanceIncrease = 0.01
+HelisChanceLimit = 0.75
 HelisChance = 0.
-assemblySAM = 300.
+assemblySAM = 600.
 
 -- Spy
 SpyChanceInitial = 0.
-SpyChanceIncrease = 0.05
-SpyChanceLimit = 3.
+SpyChanceIncrease = 0.01
+SpyChanceLimit = 0.75
 SpyChance = 0.
 timeLitmitSpy = 900.
 
@@ -64,17 +118,72 @@ function increaseTan(t)
 end
 
 -- Figther Sweeep Calling
-function eFS ()
+function eFSDummy ()
     runDice()
     local sorteEFS = mist.random(10000)/100.
-    if figtherSweep > sorteEFS then
-        AARoute = mist.getGroupRoute("E_FS")
-        FSGroup = mist.cloneGroup("E_FS",true)
+    if figtherSweepDummy > sorteEFS then
+        AARoute = mist.getGroupRoute("E_FS_Dummy")
+        FSGroup = mist.cloneGroup("E_FS_Dummy",true)
         mist.goRoute(FSGroup,AARoute)
-        figtherSweep = figtherSweepInitial
+        figtherSweepDummy = figtherSweepDummyInitial
     else
-        if figtherSweep < figtherSweepLimit then
-            figtherSweep = figtherSweep + figtherSweepIncrease
+        if figtherSweepDummy < figtherSweepDummyLimit then
+            figtherSweepDummy = figtherSweepDummy + figtherSweepDummyIncrease
+        else 
+            figtherSweepDummy = figtherSweepDummyLimit
+        end
+    end
+end
+
+function eFSLow ()
+    runDice()
+    local sorteEFS = mist.random(10000)/100.
+    if figtherSweepLow > sorteEFS then
+        AARoute = mist.getGroupRoute("E_FS_Low")
+        FSGroup = mist.cloneGroup("E_FS_Low",true)
+        mist.goRoute(FSGroup,AARoute)
+        figtherSweepLow = figtherSweepLowInitial
+    else
+        if figtherSweepLow < figtherSweepLowLimit then
+            figtherSweepLow = figtherSweepLow + figtherSweepLowIncrease
+        else 
+            figtherSweepLow = figtherSweepLowLimit
+        end
+    end
+end
+
+function eFSMedium ()
+    runDice()
+    local sorteEFS = mist.random(10000)/100.
+    if figtherSweepMedium > sorteEFS then
+        AARoute = mist.getGroupRoute("E_FS_Medium")
+        FSGroup = mist.cloneGroup("E_FS_Medium",true)
+        mist.goRoute(FSGroup,AARoute)
+        figtherSweepMedium = figtherSweepMediumInitial
+    else
+        if figtherSweepMedium < figtherSweepMediumLimit then
+            figtherSweepMedium = figtherSweepMedium + figtherSweepMediumIncrease
+        else 
+            figtherSweepMedium = figtherSweepMediumLimit
+        end
+    end
+end
+
+function eFSHigh ()
+    runDice()
+    local sorteEFS = mist.random(10000)/100.
+    local groupNameFS = math.random(1,2)
+    local groupNaameList = {"E_FS_High1","E_FS_High2"}
+    if figtherSweepHigh > sorteEFS then
+        AARoute = mist.getGroupRoute(groupNaameList[groupNameFS])
+        FSGroup = mist.cloneGroup(groupNaameList[groupNameFS],true)
+        mist.goRoute(FSGroup,AARoute)
+        figtherSweepHigh = figtherSweepHighInitial
+    else
+        if figtherSweepHigh < figtherSweepHighLimit then
+            figtherSweepHigh = figtherSweepHigh + figtherSweepHighIncrease
+        else 
+            figtherSweepHigh = figtherSweepHighLimit
         end
     end
 end
@@ -225,7 +334,7 @@ function resultHeli (event)
     if event.id == world.event.S_EVENT_LAND then
         local initNome = event.initiator
         if initNome and initNome:getGroup() then
-            if string.find(initNome:getName(), chopper.units[1].name) then
+            if string.find(initNome:getName(), chopper.units[1].name) and mist.utils.get2DDist(initPointHeliMis,mist.getAvgPos({chopper.units[1].name})) < 10.0 then
                 local mens = {}
                 mens.text = "O helicoptero chegou ao seu terrível destino. Os soldados iniciam o descarregamento e a montagem do equipamento."
                 mens.displayTime = 30
@@ -240,7 +349,7 @@ end
 
 -- Terminou o Descarregamento
 function deadHelis (event)
-    if event.id == world.event.S_EVENT_CRASH or event.id == world.event.S_EVENT_EJECTION or event.id == world.event.S_EVENT_PILOT_DEAD then
+    if event.id == world.event.S_EVENT_CRASH or event.id == world.event.S_EVENT_EJECTION or event.id == world.event.S_EVENT_PILOT_DEAD or (event.id == world.event.S_EVENT_LAND and mist.utils.get2DDist(initPointHeliMis,mist.getAvgPos({chopper.units[1].name})) > 10.0) then
         local initDead = event.initiator
         if initDead and initDead:getGroup() then
             if string.find(initDead:getName(), chopper.units[1].name)  then
@@ -250,6 +359,9 @@ function deadHelis (event)
                 mens.msgFor = {coa = {'blue'}} 
                 mens.sound = "victory.wav"
                 mist.message.add (mens)
+                if casualHelisSAMAssembling then
+                    mist.removeFunction(casualHelisSAMAssembling)
+                end
                 Group.getByName(eOutpostActor.name):destroy()
                 mist.removeEventHandler(victoryHeli)
                 mist.removeEventHandler(Heliev)
@@ -478,35 +590,117 @@ function casualEv ()
     end
 end
 
--- Player Pool Check
-function checkPlayers (event)
-    if event.id == world.event.S_EVENT_PLAYER_ENTER_UNIT then
+-- Helicopter Missions
+function checkHelipad (event)
+    if event.id == world.event.S_EVENT_LAND and event.place ~= nil then
         unidade = event.initiator
+        placeHeli = event.place
         local mens = {}
-        mens.text = unidade:getPlayerName() .. " assumiu o controle de um " .. unidade:getTypeName() .. "\nOs inimigos estao ansiosos para abate-lo e quanto mais frustrado ele ficar, mais realizados eles se sentirao."
+        mens.text = unidade:getTypeName() .. " pousou em " .. placeHeli:getName()
         mens.displayTime = 30
         mens.msgFor = {coa = {'all'}} 
         mens.sound = "Mantis.wav"
-        mist.message.add (mens)
-        table.insert(playerPool,unidade)    
+        mist.message.add(mens)
+    end
+end
+
+function updateOdds()
+    local increaseDummy = 0.0
+    local limitDummy = 0.
+    local increaseLow = 0.0
+    local limitLow = 0.
+    local increaseMedium = 0.0
+    local limitMedium = 0.
+    local increaseHigh = 0.0
+    local limitHigh = 0.
+    -- Adjusting Fighter Sweep - Dummy
+    for i,j in pairs(playerPool) do
+        increaseDummy = increaseDummy + cPlanes[j][1]*figtherSweepDummyIncreasePP
+        limitDummy = limitDummy + cPlanes[j][1]*figtherSweepDummyLimitPP
+    end
+    -- Adjusting Fighter Sweep - Low
+    for i,j in pairs(playerPool) do
+        increaseLow = increaseLow + cPlanes[j][2]*figtherSweepLowIncreasePP
+        limitLow = limitLow + cPlanes[j][2]*figtherSweepLowLimitPP
+    end
+    -- Adjusting Fighter Sweep - Medium
+    for i,j in pairs(playerPool) do
+        increaseMedium = increaseMedium + cPlanes[j][3]*figtherSweepMediumIncreasePP
+        limitMedium = limitMedium + cPlanes[j][3]*figtherSweepMediumLimitPP
+    end
+    -- Adjusting Fighter Sweep - High
+    for i,j in pairs(playerPool) do
+        increaseHigh = increaseHigh + cPlanes[j][4]*figtherSweepHighIncreasePP
+        limitHigh = limitHigh + cPlanes[j][4]*figtherSweepHighLimitPP
+    end
+    -- Adjusting Fighter Sweep - Dummy
+    figtherSweepDummyIncrease = increaseDummy
+    figtherSweepDummyLimit = limitDummy
+    figtherSweepLowIncrease = increaseLow
+    figtherSweepLowLimit = limitLow
+    figtherSweepMediumIncrease = increaseMedium
+    figtherSweepMediumLimit = limitMedium
+    figtherSweepHighIncrease = increaseHigh
+    figtherSweepHighLimit = limitHigh
+    mist.removeFunction(schedulerEFSDummy)
+    mist.removeFunction(schedulerEFSLow)
+    mist.removeFunction(schedulerEFSMedium)
+    mist.removeFunction(schedulerEFSHigh)
+    schedulerEFSDummy = mist.scheduleFunction(eFSDummy,{},timer.getTime() + 1,figtherSweepDummyInterval) -- Cacas de FS Dummy
+    schedulerEFSLow = mist.scheduleFunction(eFSLow,{},timer.getTime() + 1,figtherSweepLowInterval) -- Cacas de FS Low
+    schedulerEFSMedium = mist.scheduleFunction(eFSMedium,{},timer.getTime() + 1,figtherSweepMediumInterval) -- Cacas de FS Medium
+    schedulerEFSHigh = mist.scheduleFunction(eFSHigh,{},timer.getTime() + 1,figtherSweepHighInterval) -- Cacas de FS High
+    
+    local mens = {}
+    mens.text = "Ajustado! \nLimite Dummy: " .. figtherSweepDummyLimit .. " Increase: " .. figtherSweepDummyIncrease .. "\nLimite Low: " .. figtherSweepLowLimit .. " Increase: " .. figtherSweepLowIncrease .. "\nLimite Medium: " .. figtherSweepMediumLimit .. " Increase: " .. figtherSweepMediumIncrease .. "\nLimite High: " .. figtherSweepHighLimit .. " Increase: " .. figtherSweepHighIncrease
+    mens.displayTime = 30
+    mens.msgFor = {coa = {'all'}} 
+    mens.sound = "Mantis.wav"
+    mist.message.add(mens)
+
+end
+
+-- Player Pool Check
+function checkPlayers (event)
+    if event.id == world.event.S_EVENT_BIRTH then
+        checkID = event.initiator
+        if mist.DBs.humansByName[checkID:getName()] then
+            if mist.DBs.humansByName[checkID:getName()].skill == "Client" then
+                if playerPool[event.initiator:getPlayerName()] == nil then
+                    unidade = event.initiator
+                    local mens = {}
+                    mens.text = unidade:getPlayerName() .. " assumiu o controle de um " .. unidade:getTypeName() .. "\nOs inimigos estao ansiosos para abate-lo e quanto mais frustrado ele ficar, mais realizados eles se sentirao."
+                    mens.displayTime = 30
+                    mens.msgFor = {coa = {'all'}} 
+                    mens.sound = "Mantis.wav"
+                    mist.message.add (mens)
+                    playerPool[unidade:getPlayerName()] = unidade:getTypeName()
+                    updateOdds()
+                end
+            end
+        end
     end
 
     if event.id == world.event.S_EVENT_PLAYER_LEAVE_UNIT then
-        unidade = event.initiator
-        local mens = {}
-        mens.text = unidade:getPlayerName() .. " deixou o " .. unidade:getTypeName() .. "\nOs inimigos sentirao saudades desse aciduo fregues."
-        mens.displayTime = 30
-        mens.msgFor = {coa = {'all'}} 
-        mens.sound = "Mantis.wav"
-        mist.message.add (mens)
-        for k,v in pairs(playerPool) do
-           if v == unidade then
-                table.remove(playerPool,k)
-                break
-           end
-        end 
+        checkID = event.initiator
+        if mist.DBs.humansByName[checkID:getName()] then
+            if mist.DBs.humansByName[checkID:getName()].skill == "Client" then
+                if playerPool[event.initiator:getPlayerName()] ~= nil then
+                    unidade = event.initiator
+                    local mens = {}
+                    mens.text = unidade:getPlayerName() .. " deixou o " .. unidade:getTypeName() .. "\nOs inimigos sentirao saudades desse aciduo fregues."
+                    mens.displayTime = 30
+                    mens.msgFor = {coa = {'all'}} 
+                    mens.sound = "Mantis.wav"
+                    mist.message.add (mens)
+                    playerPool[unidade:getPlayerName()] = Null
+                end
+            end
+        end
     end
 end
+
+
 
 ------------------------------------------------------------- Main --------------------------------------------------------------------
 for i,v in ipairs(eGroups) do for j = 1,eNum[i] do mist.cloneInZone(v,eZones) end end -- Distribuindo os Inimigos
@@ -514,6 +708,11 @@ flagEvento = false
 playerPool = {}
 
 figtherSweep = figtherSweepInitial -- Prob Inicial
-mist.scheduleFunction(eFS,{},timer.getTime() + 1,figtherSweepInterval) -- Cacas de FS
+
+schedulerEFSDummy = mist.scheduleFunction(eFSDummy,{},timer.getTime() + 1,figtherSweepDummyInterval) -- Cacas de FS Dummy
+schedulerEFSLow = mist.scheduleFunction(eFSLow,{},timer.getTime() + 1,figtherSweepLowInterval) -- Cacas de FS Low
+schedulerEFSMedium = mist.scheduleFunction(eFSMedium,{},timer.getTime() + 1,figtherSweepMediumInterval) -- Cacas de FS Medium
+schedulerEFSHigh = mist.scheduleFunction(eFSHigh,{},timer.getTime() + 1,figtherSweepHighInterval) -- Cacas de FS High
+
 mist.scheduleFunction(casualEv,{},timer.getTime() + 5,casualEventsInterval) -- Secondary Missions
 checkPl = mist.addEventHandler(checkPlayers) -- PlayerPool
